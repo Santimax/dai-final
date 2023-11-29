e8c50ba1aabf5938c39afe2ddfdd8e21840d4917
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-29 09:45:02 -0300

    final listo dai

--- ss/Screens/NotificationScreen.jsx
+++ ss/Screens/NotificationScreen.jsx
@@ -1,45 +1,23 @@
 import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, Button } from 'react-native';
 import * as Notifications from 'expo-notifications';
 
-const NotificationScreen = () => {
-  const handleNotification = async () => {
-    try {
-      // Pide permiso al usuario para enviar notificaciones
-      const { status } = await Notifications.requestPermissionsAsync();
-      if (status !== 'granted') {
-        alert('La aplicación no tiene permisos para enviar notificaciones');
-        return;
-      }
-
-      // Crea y envía una notificación push
-      await Notifications.scheduleNotificationAsync({
-        content: {
-          title: '¡Hola!',
-          body: 'Esta es una notificación de prueba.',
-        },
-        trigger: null, // Envía la notificación inmediatamente
-      });
-    } catch (error) {
-      console.error('Error al enviar la notificación:', error);
-    }
+const NotificationScreen = () =>{
+  const handleNotification = () => {
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Hola',
+        body: '¡Hola desde la notificación push!',
+      },
+      trigger: null,
+    });
   };
 
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Button title="Enviar Notificación" onPress={handleNotification} />
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-
-export default NotificationScreen;
+export default NotificationScreen
